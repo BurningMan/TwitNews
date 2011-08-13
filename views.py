feab65cf98f8935f178e1429b6af474695f60ee3
Sergey Parakhin <vortexdev@gmail.com>
2011-08-13 20:48:09 +0300

    _

--- views.py
+++ views.py
@@ -16,7 +16,7 @@ def categories(request):
 
 # Fill database (default data)
 def fillDatabase(request):
-    for i in range(1, 16):
+    for i in range(1, 11):
         Tweet.objects.create(text='Text of tweet',
             author='Author of tweet',
             category='IT',
